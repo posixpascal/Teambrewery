43e1b5e31cec7e1cec253517a4ab0f71d8e4b4d5
Pascal Raszyk <pr@mediatrooper.de>
2014-11-24 01:01:25 +0100

    yo added settings, typechart, different views
    and a LOT of other stuff :) enjoy

--- client/app/team/team.controller.js
+++ client/app/team/team.controller.js
@@ -5,10 +5,37 @@ angular.module('teambreweryApp')
       $scope.team = []; 
       $scope.types = Object.keys(typeChart);
     
+     
+      $scope.saveSettings = function(s){
+          $scope.settings = angular.copy(s);
+          $.jStorage.set('settings', $scope.settings);
+      }
+    
       $scope.load = function(){
+          var default_settings = {
+              typeChart: {
+                  showPokemon: true,
+                  tableMode: "types",
+                  rows: {
+                      immunity: true,
+                      neutral: true,
+                      effective: true,
+                      superEffective: true,
+                      resistant: true,
+                      quadResistant: true
+                  },
+              }
+          }
+          
+          $scope.settings = $.jStorage.get('settings', default_settings);
+          $scope.customSettings = angular.copy($scope.settings);
+          
+          $scope.$watch(function(){
+              return $scope.team.length;
+          }, $scope.getTeamWeakness);
+          
           if ($stateParams.id){ 
               $http.get('/api/team/get/' + $stateParams.id).success(function(data){
-
                   $scope.team = JSON.parse(data[0].team);
               });
           }
@@ -33,6 +60,9 @@ angular.module('teambreweryApp')
           }
       }
       
+      $scope.clearTeam = function(){
+          $scope.team = [];
+      }
 
       $scope.addPokemon = function(){
 
@@ -64,38 +94,154 @@ angular.module('teambreweryApp')
   
       $scope.saveTeam = function(){
           Team.save($scope.team);
-          
       }
-
-
-      $scope.getImmunity = function(type){
-          var immunity = 0;
-          angular.forEach($scope.team, function(pokemon){
-              angular.forEach(pokemon.types, function(theType){
-                  if (typeChart[theType].damageTaken[type] == 3){
-                      immunity++;
-                  }
-              })
-          });
-          return immunity;
-      };
       
-      $scope.getQuadResistance = function(type){
-          var quadResistance = 0;
-          angular.forEach($scope.team, function(pokemon){
-              var resistent = 0;
-              
-              angular.forEach(pokemon.types, function(theType){
-                  if (typeChart[theType].damageTaken[type] == 3) return;
-                  else if (typeChart[theType].damageTaken[type] == 2){
-                      resistent++;
+      $scope.teamWeaknesses = function(type){
+          if ($scope.weakness){
+              var typeWeakness = $scope.weakness[type];
+              return _.flatten([typeWeakness.effective, typeWeakness.superEffective])
+                  
+          } else {
+              $scope.getTeamWeaknesses();
+              return $scope.teamWeaknesses(type);
+          }
+      }
+      $scope.teamResistance = function(type){
+          if ($scope.weakness){
+              var typeWeakness = $scope.weakness[type];
+              return _.flatten([typeWeakness.resist, typeWeakness.quadResist])
+                  
+          } else {
+              $scope.getTeamWeaknesses();
+              return $scope.teamResistance(type);
+          }
+      }
+      
+      $scope.teamImmunities = function(type){
+          if ($scope.weakness){
+              var typeWeakness = $scope.weakness[type];
+              return typeWeakness.immunity;
+                  
+          } else {
+              $scope.getTeamWeaknesses();
+              return $scope.teamImmunities(type);
+          }
+      }
+      
+      $scope.getWeaknessesFor = function(pokemon){
+          var weaknesses = {};
+
+          var types = [];
+          types.push(pokemon.types[0]);
+          if (typeof pokemon.types[1] !== "undefined") types.push(pokemon.types[1]);
+          else types.push('Bird');
+          
+          angular.forEach(types, function(pokeType){
+              if (pokeType == 'Bird') return; // luls. bird.
+              angular.forEach(Object.keys(typeChart[pokeType].damageTaken), function(theType){
+                  var weaknessLevel = typeChart[pokeType].damageTaken[theType];
+                  if (weaknessLevel == 1){
+                      weaknesses[theType] = true;
                   } 
               });
-              
-              if (resistent == 2) quadResistance++;
           });
-          return quadResistance;
-      };
+              
+
+          var realWeaknesses = [];
+          
+          // premodify weakness object, because types overwrite each other. so we have to check that.
+          angular.forEach(Object.keys(weaknesses), function(weaknessType){
+              
+              var isRealWeakness = true;
+
+              angular.forEach(types, function(pokeType){
+                  var damageTaken = typeChart[pokeType].damageTaken[weaknessType];
+                  if (damageTaken >= 2){
+                      isRealWeakness = false;
+                  }
+              });
+              
+              if (isRealWeakness){
+                  realWeaknesses.push(weaknessType);
+              }
+          });
+
+
+          return realWeaknesses;
+              
+      }
+      
+      $scope.getTeamWeakness = function(){
+          var weakness = {};
+          
+          // for each type:
+          angular.forEach(Object.keys(typeChart), function(type){
+              weakness[type] = {
+                  immunity: [],
+                  quadResist: [],
+                  resist: [],
+                  neutral: [],
+                  effective: [],
+                  superEffective: []
+              };
+              
+              // for each pokemon in team
+              angular.forEach($scope.team, function(pokemon){
+                  var types = [];
+                  types.push(pokemon.types[0]);
+                  if (typeof pokemon.types[1] !== "undefined") types.push(pokemon.types[1]);
+                  else types.push('Bird');
+                  
+                  
+                  var type1 = (typeChart[types[0]].damageTaken[type]);
+                  var type2 = (typeChart[types[1]].damageTaken[type]);
+                  var immunity = (type1 == 3 || type2 == 3)
+                      
+                 // check for immunity:
+                 if (immunity){
+                     weakness[type]['immunity'].push(pokemon);
+                 }
+                 
+                 // check for quad resist
+                 if ( !immunity && (
+                     type1 == 2 && 
+                     type2 == 2)
+                 ) weakness[type]['quadResist'].push(pokemon);
+                 
+                 // resist
+                 var resistent = false;
+                 if ( !immunity && (
+                     (type1 == 2 && type2 == 0) ||
+                     (type2 == 2 && type1 == 0))) resistent = true;
+
+                 if (resistent)
+                         weakness[type]['resist'].push(pokemon);
+                     
+                 // neutral
+                 var neutral = (type1 == 0 && type2 == 0);
+                 if ( !immunity &&
+                     (type1 == 0 && type2 == 0)
+                 ) weakness[type]['neutral'].push(pokemon);
+                 
+                 
+                 // check for super effective
+                 if ( !immunity && 
+                     (type1 == 1 && type2 == 1)
+                 ) weakness[type]['superEffective'].push(pokemon);
+                 
+                 // effective
+                 if ( !immunity && (
+                     (type1 == 1 && type2 == 0) ||
+                     (type2 == 1 && type1 == 0))
+                 ) weakness[type]['effective'].push(pokemon);
+                 
+              });
+          });
+          
+          
+          $scope.weakness = weakness;
+      }
+
 
       
       
--- client/app/team/team.directive.js
+++ client/app/team/team.directive.js
@@ -158,6 +158,28 @@ damageTaken: {
 },
 HPivs: {"def":30, "spa":30, "spd":30, "spe":30}
 },
+"Bird": {
+    damageTaken: {
+        "Bug": 0,
+        "Dark": 0,
+        "Dragon": 0,
+        "Electric": 0,
+        "Fairy": 0,
+        "Fighting": 0,
+        "Fire": 0,
+        "Flying": 0,
+        "Ghost": 0,
+        "Grass": 0,
+        "Ground": 0,
+        "Ice": 0,
+        "Normal": 0,
+        "Poison": 0,
+        "Psychic": 0,
+        "Rock": 0,
+        "Steel": 0,
+        "Water": 0
+    }
+},
 "Fire": {
 damageTaken: {
 brn: 3,
