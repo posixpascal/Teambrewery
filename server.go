41043d39b1ad61681bb0319175114215d6f59389
Pascal Raszyk <pr@mediatrooper.de>
2014-12-22 17:39:14 +0100

    added document-mode to server.

--- server.go
+++ server.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"net/http"
 	"github.com/go-martini/martini"
 )
 
 func main(){
 	m := martini.Classic();
-	
 
-	
+	static := martini.Static("client", martini.StaticOptions{Fallback: "/index.html", Exclude: "/api/v"})
+	m.NotFound(static, http.NotFound)
+
 	m.Use(martini.Static("client"))
 	m.Use(martini.Static("client/app"))
 	m.Run()
